c8ecc279aa492799b10c51fcb05f1a8d3a9fad27
Eddie Hung <eddie@fpgeh.com>
2020-05-12 15:10:08 -0700

    ice40: map $__ABC_PACK_44 into ICESTORM_LC using LO

--- techlibs/ice40/cells_map.v
+++ techlibs/ice40/cells_map.v
@@ -60,3 +60,68 @@ module \$lut (A, Y);
   endgenerate
 endmodule
 `endif
+
+module \$__ABC9_PACK_44 (input [A_WIDTH-1:0] A, input [B_WIDTH-1:0] B, output X, Y);
+  parameter A_WIDTH = 0;
+  parameter A_LUT = 0;
+  parameter B_WIDTH = 0;
+  parameter B_LUT = 0;
+
+  parameter _TECHMAP_BITS_CONNMAP_ = 0;
+  parameter [B_WIDTH*_TECHMAP_BITS_CONNMAP_-1:0] _TECHMAP_CONNMAP_B_ = 0;
+  parameter [_TECHMAP_BITS_CONNMAP_-1:0] _TECHMAP_CONNMAP_X_ = 0;
+
+  generate
+    if (A_WIDTH == 2) begin
+      localparam [15:0] A_LUT2 = {{4{A_LUT[3]}}, {4{A_LUT[2]}}, {4{A_LUT[1]}}, {4{A_LUT[0]}}};
+      \$__ABC9_PACK_44 #(.A_WIDTH(4), .A_LUT(A_LUT2), .B_WIDTH(B_WIDTH), .B_LUT(B_LUT))
+        _TECHMAP_REPLACE_ (.A({A[1],A[0],1'b0,1'b0}), .B(B), .X(X), .Y(Y));
+    end
+    else if (A_WIDTH == 3) begin
+      localparam [15:0] A_LUT2 = {{2{A_LUT[7]}}, {2{A_LUT[6]}}, {2{A_LUT[5]}}, {2{A_LUT[4]}}, {2{A_LUT[3]}}, {2{A_LUT[2]}}, {2{A_LUT[1]}}, {2{A_LUT[0]}}};
+      \$__ABC9_PACK_44 #(.A_WIDTH(4), .A_LUT(A_LUT2), .B_WIDTH(B_WIDTH), .B_LUT(B_LUT))
+        _TECHMAP_REPLACE_ (.A({A[2],A[1],A[0],1'b0}), .B(B), .X(X), .Y(Y));
+    end
+    else if (B_WIDTH == 2) begin
+      localparam [15:0] B_LUT2 = {{4{B_LUT[3]}}, {4{B_LUT[2]}}, {4{B_LUT[1]}}, {4{B_LUT[0]}}};
+      \$__ABC9_PACK_44 #(.A_WIDTH(4), .A_LUT(A_LUT), .B_WIDTH(4), .B_LUT(B_LUT2))
+        _TECHMAP_REPLACE_ (.A(A), .B({B[1],B[0],1'b0,1'b0}), .X(X), .Y(Y));
+    end
+    else if (B_WIDTH == 3) begin
+      localparam [15:0] B_LUT2 = {{2{B_LUT[7]}}, {2{B_LUT[6]}}, {2{B_LUT[5]}}, {2{B_LUT[4]}}, {2{B_LUT[3]}}, {2{B_LUT[2]}}, {2{B_LUT[1]}}, {2{B_LUT[0]}}};
+      \$__ABC9_PACK_44 #(.A_WIDTH(4), .A_LUT(A_LUT), .B_WIDTH(4), .B_LUT(B_LUT2))
+        _TECHMAP_REPLACE_ (.A(A), .B({B[2],B[1],B[0],1'b0}), .X(X), .Y(Y));
+    end
+    else if (A_WIDTH == 4 && B_WIDTH == 4) begin
+      wire LO;
+      ICESTORM_LC #(.LUT_INIT(A_LUT)) a_lut (.I0(A[0]), .I1(A[1]), .I2(A[2]), .I3(A[3]), .O(X), .LO(LO));
+
+      if (_TECHMAP_CONNMAP_B_[_TECHMAP_BITS_CONNMAP_*0 +: _TECHMAP_BITS_CONNMAP_] == _TECHMAP_CONNMAP_X_) begin
+        // Move I0 to I2
+        localparam [15:0] B_LUT2 = {B_LUT[15], B_LUT[11], B_LUT[14], B_LUT[10], B_LUT[13], B_LUT[9], B_LUT[12], B_LUT[8],
+          B_LUT[7], B_LUT[3], B_LUT[6], B_LUT[2], B_LUT[5], B_LUT[1], B_LUT[4], B_LUT[0] };
+        ICESTORM_LC #(.LUT_INIT(B_LUT2)) b_lut (.I0(B[1]), .I1(B[2]), .I2(LO), .I3(B[3]), .O(Y));
+      end
+      else if (_TECHMAP_CONNMAP_B_[_TECHMAP_BITS_CONNMAP_*1 +: _TECHMAP_BITS_CONNMAP_] == _TECHMAP_CONNMAP_X_) begin
+        // Move I1 to I2
+        localparam [15:0] B_LUT2 = {B_LUT[15], B_LUT[14], B_LUT[11], B_LUT[10], B_LUT[13], B_LUT[12], B_LUT[9], B_LUT[8],
+          B_LUT[7], B_LUT[6], B_LUT[3], B_LUT[2], B_LUT[5], B_LUT[4], B_LUT[1], B_LUT[0] };
+        ICESTORM_LC #(.LUT_INIT(B_LUT2)) b_lut (.I0(B[0]), .I1(B[2]), .I2(LO), .I3(B[3]), .O(Y));
+      end
+      else if (_TECHMAP_CONNMAP_B_[_TECHMAP_BITS_CONNMAP_*2 +: _TECHMAP_BITS_CONNMAP_] == _TECHMAP_CONNMAP_X_) begin
+        // Already at I2
+        ICESTORM_LC #(.LUT_INIT(B_LUT)) b_lut (.I0(B[0]), .I1(B[1]), .I2(LO), .I3(B[3]), .O(Y));
+      end
+      else if (_TECHMAP_CONNMAP_B_[_TECHMAP_BITS_CONNMAP_*3 +: _TECHMAP_BITS_CONNMAP_] == _TECHMAP_CONNMAP_X_) begin
+        // Swap I3 and I2
+        localparam [15:0] B_LUT2 = { B_LUT[15], B_LUT[7], B_LUT[13], B_LUT[5], B_LUT[11], B_LUT[3], B_LUT[9], B_LUT[1],
+          B_LUT[14], B_LUT[6], B_LUT[12], B_LUT[4], B_LUT[10], B_LUT[2], B_LUT[8], B_LUT[0] };
+        ICESTORM_LC #(.LUT_INIT(B_LUT2)) b_lut (.I0(B[0]), .I1(B[1]), .I2(LO), .I3(B[2]), .O(Y));
+      end
+      else
+        wire _TECHMAP_FAIL_ = 1;
+    end
+    else
+      wire _TECHMAP_FAIL_ = 1;
+  endgenerate
+endmodule
