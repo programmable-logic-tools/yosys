918b72027c172c7900cfd3b386abb88a7d888aa5
Eddie Hung <eddie@fpgeh.com>
2020-05-12 14:39:27 -0700

    abc9_pack: match to $__ABC9_PACK_44

--- passes/pmgen/abc9_pack.cc
+++ passes/pmgen/abc9_pack.cc
@@ -25,71 +25,62 @@ PRIVATE_NAMESPACE_BEGIN
 
 #include "passes/pmgen/abc9_pack_pm.h"
 
-void create_abc9_pack_NNN4(abc9_pack_pm &pm)
+void create_abc9_pack_44(abc9_pack_pm &pm)
 {
-	auto &st = pm.st_abc9_pack_NNN4;
+	auto &st = pm.st_abc9_pack_44;
 	log_assert(st.root);
 	log_assert(st.root->type == ID($lut));
-	log_assert(st.root->getParam(ID::WIDTH) == 4);
 
-	log("  replacing structure rooted at '%s' with $__ABC9_CLUSTER_NNN4 cell.\n", log_id(st.root));
+	log("  replacing structure rooted at '%s' with $__ABC9_PACK_44 cell.\n", log_id(st.root));
 
-	auto A = st.root->getPort(ID::A);
-	auto cell = pm.module->addCell(NEW_ID, ID($__ABC9_CLUSTER_NNN4));
-	cell->setParam(ID(B_LUT), st.root->getParam(ID::LUT));
+	auto cell = pm.module->addCell(NEW_ID, ID($__ABC9_PACK_44));
+
+	Const lut = st.root->getParam(ID::LUT);
 	cell->setPort(ID::Y, st.root->getPort(ID::Y));
 
+	auto B = st.root->getPort(ID::A);
 	if (st.A0) {
-		cell->setPort(ID(A0), st.A0->getPort(ID::A));
-		cell->setParam(ID(A0_WIDTH), st.A0->getParam(ID::WIDTH));
-		cell->setParam(ID(A0_LUT), st.A0->getParam(ID::LUT));
+		auto X = st.A0->getPort(ID::Y);
+		log_assert(B[0] == X);
+		cell->setPort(ID::A, st.A0->getPort(ID::A));
+		cell->setPort(ID::X, X);
+		cell->setParam(ID::A_WIDTH, st.A0->getParam(ID::WIDTH));
+		cell->setParam(ID(A_LUT), st.A0->getParam(ID::LUT));
 		pm.autoremove(st.A0);
+		log_cell(st.A0);
 	}
-	else {
-		// Add route-through
-		cell->setPort(ID(A0), A[0]);
-		cell->setPort(ID(A0_LUT), Const::from_string("10"));
-		cell->setParam(ID(A0_WIDTH), 1);
-	}
-
-	if (st.A1) {
-		cell->setPort(ID(A1), st.A1->getPort(ID::A));
-		cell->setParam(ID(A1_WIDTH), st.A1->getParam(ID::WIDTH));
-		cell->setParam(ID(A1_LUT), st.A1->getParam(ID::LUT));
+	else if (st.A1) {
+		auto X = st.A1->getPort(ID::Y);
+		log_assert(B[1] == X);
+		cell->setPort(ID::A, st.A1->getPort(ID::A));
+		cell->setPort(ID::X, X);
+		cell->setParam(ID::A_WIDTH, st.A1->getParam(ID::WIDTH));
+		cell->setParam(ID(A_LUT), st.A1->getParam(ID::LUT));
 		pm.autoremove(st.A1);
 	}
-	else {
-		// Add route-through
-		cell->setPort(ID(A1), A[1]);
-		cell->setPort(ID(A1_LUT), Const::from_string("10"));
-		cell->setParam(ID(A1_WIDTH), 1);
-	}
-
-	if (st.A2) {
-		cell->setPort(ID(A2), st.A2->getPort(ID::A));
-		cell->setParam(ID(A2_WIDTH), st.A2->getParam(ID::WIDTH));
-		cell->setParam(ID(A2_LUT), st.A2->getParam(ID::LUT));
+	else if (st.A2) {
+		auto X = st.A2->getPort(ID::Y);
+		log_assert(B[2] == X);
+		cell->setPort(ID::A, st.A2->getPort(ID::A));
+		cell->setPort(ID::X, X);
+		cell->setParam(ID::A_WIDTH, st.A2->getParam(ID::WIDTH));
+		cell->setParam(ID(A_LUT), st.A2->getParam(ID::LUT));
 		pm.autoremove(st.A2);
 	}
-	else {
-		// Add route-through
-		cell->setPort(ID(A2), A[2]);
-		cell->setPort(ID(A2_LUT), Const::from_string("10"));
-		cell->setParam(ID(A2_WIDTH), 1);
-	}
-
-
-	if (st.A3) {
-		cell->setPort(ID(A3), st.A3->getPort(ID::A));
-		cell->setParam(ID(A3_WIDTH), st.A3->getParam(ID::WIDTH));
-		cell->setParam(ID(A3_LUT), st.A3->getParam(ID::LUT));
+	else if (st.A3) {
+		auto X = st.A3->getPort(ID::Y);
+		log_assert(B[3] == X);
+		cell->setPort(ID::A, st.A3->getPort(ID::A));
+		cell->setPort(ID::X, X);
+		cell->setParam(ID::A_WIDTH, st.A3->getParam(ID::WIDTH));
+		cell->setParam(ID(A_LUT), st.A3->getParam(ID::LUT));
 		pm.autoremove(st.A3);
 	}
-	else {
-		// Add route-through
-		cell->setPort(ID(A3), A[3]);
-		cell->setPort(ID(A3_LUT), Const::from_string("10"));
-	}
+	else
+		log_abort();
+
+	cell->setPort(ID::B, B);
+	cell->setPort(ID::B_WIDTH, st.root->getParam(ID::WIDTH));
 
 	pm.autoremove(st.root);
 }
@@ -115,7 +106,7 @@ struct Abc9PackPass : public Pass {
 		extra_args(args, argidx, design);
 
 		for (auto module : design->selected_modules()) {
-			abc9_pack_pm(module, module->selected_cells()).run_abc9_pack_NNN4(create_abc9_pack_NNN4);
+			abc9_pack_pm(module, module->selected_cells()).run_abc9_pack_44(create_abc9_pack_44);
 		}
 	}
 } Abc9PackPass;
